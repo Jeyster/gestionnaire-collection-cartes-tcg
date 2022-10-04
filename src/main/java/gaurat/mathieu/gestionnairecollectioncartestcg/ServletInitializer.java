f76a775947a2616045f7b4365af9a570871f5a78
GAURAT Mathieu <mathieu.gaurat@soprasteria.com>
2022-10-04 13:34:15 +0200

    SpringBoot project initialisation

---
+++ src/main/java/gaurat/mathieu/gestionnairecollectioncartestcg/ServletInitializer.java
@@ -0,0 +1,13 @@
+package gaurat.mathieu.gestionnairecollectioncartestcg;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestionnairecollectioncartestcgApplication.class);
+	}
+
+}
---
+++ src/test/java/gaurat/mathieu/gestionnairecollectioncartestcg/GestionnairecollectioncartestcgApplicationTests.java
@@ -0,0 +1,13 @@
+package gaurat.mathieu.gestionnairecollectioncartestcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnairecollectioncartestcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
