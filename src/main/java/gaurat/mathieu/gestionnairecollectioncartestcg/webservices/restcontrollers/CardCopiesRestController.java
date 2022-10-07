12a3469cd4b3bbb2f397810500b7a7fe80b27db3
GAURAT Mathieu <mathieu.gaurat@soprasteria.com>
2022-10-07 12:45:54 +0200

    TU : CardCopiesRestController

--- src/main/java/gaurat/mathieu/gestionnairecollectioncartestcg/webservices/restcontrollers/CardCopiesRestController.java
+++ src/main/java/gaurat/mathieu/gestionnairecollectioncartestcg/webservices/restcontrollers/CardCopiesRestController.java
@@ -1,16 +1,19 @@
 package gaurat.mathieu.gestionnairecollectioncartestcg.webservices.restcontrollers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gaurat.mathieu.gestionnairecollectioncartestcg.model.CardCopies;
-import gaurat.mathieu.gestionnairecollectioncartestcg.webservices.dto.UserDTO;
+import gaurat.mathieu.gestionnairecollectioncartestcg.webservices.dto.CardCopiesDTO;
+import gaurat.mathieu.gestionnairecollectioncartestcg.webservices.restcontrollers.interfaces.IDTOToEntityMapping;
 import gaurat.mathieu.gestionnairecollectioncartestcg.webservices.services.implementations.CardCopiesServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,7 +23,7 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/rest/cardCopies/api")
 @Api(value = "Card Copies Rest Controller: contains all operations for managing cards copies")
-public class CardCopiesRestController {
+public class CardCopiesRestController implements IDTOToEntityMapping<CardCopiesDTO, CardCopies> {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(CardRestController.class);
     
@@ -32,11 +35,21 @@ public class CardCopiesRestController {
      * @return
      */
     @GetMapping("/all")
-    @ApiOperation(value = "Get all cards copies", response = UserDTO.class)
+    @ApiOperation(value = "Get all cards copies", response = CardCopiesDTO.class)
     @ApiResponses(value = { @ApiResponse(code = 200, message = "Ok: successfull research"),
     		@ApiResponse(code = 204, message = "No Content: no result founded") })
-    public void getAll() {
-        List<CardCopies> cardCopies = service.getCardCopies();
-        System.out.println(cardCopies);
-    }
+    public ResponseEntity<List<CardCopiesDTO>> getAll() {
+        List<CardCopies> cardsCopies = service.getCardCopies();
+        if (!cardsCopies.isEmpty()) {
+        	CardCopiesDTO cardCopiesDto;
+        	List<CardCopiesDTO> cardsCopiesDto = new ArrayList<>();
+        	for (CardCopies cardCopies : cardsCopies) {
+        		cardCopiesDto = mapEntityToDTO(cardCopies, CardCopiesDTO.class);
+        		cardsCopiesDto.add(cardCopiesDto);
+        	}
+        	
+        	return ResponseEntity.ok(cardsCopiesDto);
+        }
+
+        return ResponseEntity.noContent().build();        }
 }
---
+++ src/test/java/gaurat/mathieu/gestionnairecollectioncartestcg/webservices/restcontrollers/TestCardCopiesRestController.java
@@ -0,0 +1,122 @@
+package gaurat.mathieu.gestionnairecollectioncartestcg.webservices.restcontrollers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import gaurat.mathieu.gestionnairecollectioncartestcg.model.Card;
+import gaurat.mathieu.gestionnairecollectioncartestcg.model.CardCopies;
+import gaurat.mathieu.gestionnairecollectioncartestcg.model.Collection;
+import gaurat.mathieu.gestionnairecollectioncartestcg.model.Game;
+import gaurat.mathieu.gestionnairecollectioncartestcg.model.User;
+import gaurat.mathieu.gestionnairecollectioncartestcg.webservices.dto.CardCopiesDTO;
+import gaurat.mathieu.gestionnairecollectioncartestcg.webservices.services.implementations.CardCopiesServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+class TestCardCopiesRestController {
+
+	private static final Integer GAME_ID = 1;
+	private static final String GAME_NAME = "gameName";
+	private static final Integer CARD_ID = 1;
+	private static final String CARD_NAME = "cardName";
+	private static final Integer COLLECTION_ID = 1;
+	private static final Integer USER_ID = 1;
+	private static final String USER_NAME = "userName";
+	private static final String USER_FORNAME = "userForname";
+	private static final String USER_EMAIL = "userEmail";
+	private static final Integer CARD_COPIES_ID = 1;
+	private static final Integer CARD_COPIES_NUMBER = 4;
+
+	
+	@Mock
+	private CardCopiesServiceImpl service;
+	
+	@InjectMocks
+	private CardCopiesRestController restController;
+	
+	private static CardCopies cardCopies;
+	private static List<CardCopies> cardsCopies;
+	private static CardCopiesDTO cardCopiesDTO;
+	private static List<CardCopiesDTO> cardsCopiesDTO;
+	
+	private static Game game;
+	private static Card card;
+	private static User user;
+	private static Collection collection;
+
+	@BeforeAll
+	private static void beforeAll() {
+		game = new Game();
+		game.setIdGame(GAME_ID);
+		game.setName(GAME_NAME);
+		
+		card = new Card();
+		card.setIdCard(CARD_ID);
+		card.setName(CARD_NAME);
+		card.setGame(game);
+		
+		user = new User();
+		user.setIdUser(USER_ID);
+		user.setName(USER_NAME);
+		user.setForname(USER_FORNAME);
+		user.setEmail(USER_EMAIL);
+		
+		collection = new Collection();
+		collection.setIdCollection(COLLECTION_ID);
+		collection.setGame(game);
+		collection.setUser(user);
+		
+		cardCopies = new CardCopies();
+		cardCopies.setIdCardCopies(CARD_COPIES_ID);
+		cardCopies.setCard(card);
+		cardCopies.setCollection(collection);
+		cardCopies.setCopiesNumber(CARD_COPIES_NUMBER);
+		
+		cardsCopies = new ArrayList<>();
+		cardsCopies.add(cardCopies);
+		
+		cardCopiesDTO = new CardCopiesDTO();
+		cardCopiesDTO.setIdCardCopies(CARD_COPIES_ID);
+		cardCopiesDTO.setIdCard(CARD_ID);
+		cardCopiesDTO.setIdCollection(COLLECTION_ID);
+		cardCopiesDTO.setCopiesNumber(CARD_COPIES_NUMBER);
+		
+		cardsCopiesDTO = new ArrayList<>();
+		cardsCopiesDTO.add(cardCopiesDTO);
+	}
+
+	@Test
+	@DisplayName("mapEntityToDTO : CardCopies is converted in CardCopiesDTO")
+	void testMapEntityToDTO_conversionOk() {	
+		CardCopiesDTO cardCopiesDTOToTest = restController.mapEntityToDTO(cardCopies, CardCopiesDTO.class);
+		assertEquals(cardCopiesDTO, cardCopiesDTOToTest);
+	}
+	
+	@Test
+	@DisplayName("getAll : get all CardCopies and returns OK")
+	void testGetAll_returnsAllCardCopiesOk() {
+		ResponseEntity<List<CardCopiesDTO>> responseExpected = ResponseEntity.ok(cardsCopiesDTO);
+		Mockito.when(service.getCardCopies()).thenReturn(cardsCopies);
+		assertEquals(responseExpected, restController.getAll());
+	}
+	
+	@Test
+	@DisplayName("getAll : get no CardCopies and returns NO CONTENT")
+	void testGetAll_returnsNoCardCopiesKo() {
+		ResponseEntity<List<CardCopiesDTO>> responseExpected = ResponseEntity.noContent().build();
+		Mockito.when(service.getCardCopies()).thenReturn(new ArrayList<>());
+		assertEquals(responseExpected, restController.getAll());
+	}
+
+}
